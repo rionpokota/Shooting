d713a225d32789cd099a7f24e9551c0ede5de275
rion96194 <rion_96194_y@yahoo.co.jp>
2023-01-03 09:46:21 +0900

    update

---
+++ Shooting.py
@@ -0,0 +1,383 @@
+from random import random
+import pyxel
+
+# 画面
+SCENE_TITLE = 0
+SCENE_PLAY = 1
+SCENE_GAMEOVER = 2
+
+# 星の数、手前の星の色、奥の星の色
+STAR_COUNT = 50
+STAR_COLOR_HIGH = 10
+STAR_COLOR_LOW = 4
+
+# プレイヤーの大きさ、動きの速さ
+PLAYER_WIDTH = 8
+PLAYER_HEIGHT = 8
+PLAYER_SPEED = 2
+
+# 弾の大きさ、色、速さ、発射間隔（大きいほど間隔が長い）
+BULLET_WIDTH = 2
+BULLET_HEIGHT = 5
+BULLET_COLOR = 7
+BULLET_SPEED = 4
+BULLET_INTERVAL = 6
+
+# 敵の大きさ、速さ、出現率（小さいほど多い）
+ENEMY_WIDTH = 8
+ENEMY_HEIGHT = 8
+ENEMY_SPEED = 1
+ENEMY_APPEAR = 6
+
+# 爆発の大きさ
+BLAST_START_RADIUS = 1
+BLAST_END_RADIUS = 8
+BLAST_COLOR_IN = 7
+BLAST_COLOR_OUT = 10
+
+# 敵のリスト
+enemy_list = []
+# 弾のリスト
+bullet_list = []
+# 爆発のリスト
+blast_list = []
+
+
+# 更新
+def update_list(lists):
+    for elem in lists:
+        elem.update()
+
+
+# 描画
+def draw_list(lists):
+    for elem in lists:
+        elem.draw()
+
+
+# 初期化
+def cleanup_list(lists):
+    i = 0
+    while i < len(lists):
+        elem = lists[i]
+        if not elem.alive:
+            lists.pop(i)
+        else:
+            i += 1
+
+
+# 背景
+class Background:
+    def __init__(self):
+        self.star_list = []
+        for i in range(STAR_COUNT):
+            self.star_list.append(
+                (random() * pyxel.width, random() * pyxel.height, random() * 1.5 + 1)
+            )
+
+    def update(self):
+        for i, (x, y, speed) in enumerate(self.star_list):
+            y += speed
+            if y >= pyxel.height:
+                y -= pyxel.height
+            self.star_list[i] = (x, y, speed)
+
+    def draw(self):
+        for (x, y, speed) in self.star_list:
+            pyxel.pset(x, y, STAR_COLOR_HIGH if speed > 1.8 else STAR_COLOR_LOW)
+
+
+# プレイヤー
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = PLAYER_WIDTH
+        self.h = PLAYER_HEIGHT
+        self.alive = True
+        self.i = 0
+
+    def update(self):
+        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT):
+            self.x -= PLAYER_SPEED
+
+        if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT):
+            self.x += PLAYER_SPEED
+
+        if pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
+            self.y -= PLAYER_SPEED
+
+        if pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN):
+            self.y += PLAYER_SPEED
+
+        self.x = max(self.x, 0)
+        self.x = min(self.x, pyxel.width - self.w)
+        self.y = max(self.y, 0)
+        self.y = min(self.y, pyxel.height - self.h)
+
+        # self.i += 1
+        # if pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_A):
+        #     # 6フレーム目に発射するが、連打した時に6フレーム外だと発射されない……。
+        #     if self.i % BULLET_INTERVAL == 0:
+        #         Bullet(
+        #             self.x + (PLAYER_WIDTH - BULLET_WIDTH) / 2, self.y - BULLET_HEIGHT / 2
+        #         )
+        #         pyxel.play(0, 0)
+
+        # 連打でも長押しでも弾が発射されるようにした。
+        if pyxel.btnp(pyxel.KEY_A, 1, BULLET_INTERVAL) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A, 1, BULLET_INTERVAL):
+            Bullet(
+                self.x + (PLAYER_WIDTH - BULLET_WIDTH) / 2, self.y - BULLET_HEIGHT / 2
+            )
+            pyxel.play(0, 0)
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, 0, 0, self.w, self.h, 0)
+
+
+# 弾
+class Bullet:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = BULLET_WIDTH
+        self.h = BULLET_HEIGHT
+        self.alive = True
+
+        bullet_list.append(self)
+
+    def update(self):
+        self.y -= BULLET_SPEED
+
+        if self.y + self.h - 1 < 0:
+            self.alive = False
+
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.w, self.h, BULLET_COLOR)
+
+
+# 敵
+class Enemy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = ENEMY_WIDTH
+        self.h = ENEMY_HEIGHT
+        self.dir = 1
+        self.alive = True
+        self.offset = int(random() * 60)
+
+        enemy_list.append(self)
+
+    def update(self):
+        if (pyxel.frame_count + self.offset) % 60 < 30:
+            self.x += ENEMY_SPEED
+            self.dir = 1
+
+        else:
+            self.x -= ENEMY_SPEED
+            self.dir = -1
+
+        self.y += ENEMY_SPEED
+
+        if self.y > pyxel.height - 1:
+            self.alive = False
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, 8, 0, self.w * self.dir, self.h, 0)
+
+
+# 爆発
+class Blast:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.radius = BLAST_START_RADIUS
+        self.alive = True
+
+        blast_list.append(self)
+
+    def update(self):
+        self.radius += 1
+
+        if self.radius > BLAST_END_RADIUS:
+            self.alive = False
+
+    def draw(self):
+        pyxel.circ(self.x, self.y, self.radius, BLAST_COLOR_IN)
+        pyxel.circb(self.x, self.y, self.radius, BLAST_COLOR_OUT)
+
+
+class App:
+    def __init__(self):
+        pyxel.init(120, 160, title="Shooting")
+
+        # 自機のイメージ
+        pyxel.image(0).set(
+            0,
+            0,
+            [
+                "00c00c00",
+                "0c7007c0",
+                "0c7007c0",
+                "c703b07c",
+                "77033077",
+                "785cc587",
+                "85c77c58",
+                "0c0880c0",
+            ],
+        )
+
+        # 敵のイメージ
+        pyxel.image(0).set(
+            8,
+            0,
+            [
+                "00888800",
+                "08888880",
+                "868a8a88",
+                "888a8a88",
+                "88888888",
+                "08880880",
+                "00888800",
+                "08808800",
+
+            ],
+        )
+
+        # サウンド
+        pyxel.sound(0).set("a3a2c1a1", "p", "7", "s", 5)
+        pyxel.sound(1).set("a3a2c2c2", "n", "7742", "s", 10)
+
+        self.scene = SCENE_TITLE
+        self.score = 0
+        self.life = 3
+        self.background = Background()
+        self.player = Player(pyxel.width / 2, pyxel.height - 20)
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        self.background.update()
+
+        if self.scene == SCENE_TITLE:
+            self.update_title_scene()
+        elif self.scene == SCENE_PLAY:
+            self.update_play_scene()
+        elif self.scene == SCENE_GAMEOVER:
+            self.update_gameover_scene()
+
+    def update_title_scene(self):
+        if pyxel.btnp(pyxel.KEY_RETURN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_B):
+            self.scene = SCENE_PLAY
+
+    def update_play_scene(self):
+        if pyxel.frame_count % ENEMY_APPEAR == 0:
+            Enemy(random() * (pyxel.width - PLAYER_WIDTH), 0)
+
+        for a in enemy_list:
+            for b in bullet_list:
+                if (
+                        a.x + a.w > b.x
+                        and b.x + b.w > a.x
+                        and a.y + a.h > b.y
+                        and b.y + b.h > a.y
+                ):
+                    a.alive = False
+                    b.alive = False
+
+                    # 自機の爆発を生成する
+                    blast_list.append(
+                        Blast(a.x + ENEMY_WIDTH / 2, a.y + ENEMY_HEIGHT / 2)
+                    )
+
+                    pyxel.play(1, 1)
+                    self.score += 10
+
+        for enemy in enemy_list:
+            if (
+                    self.player.x + self.player.w > enemy.x
+                    and enemy.x + enemy.w > self.player.x
+                    and self.player.y + self.player.h > enemy.y
+                    and enemy.y + enemy.h > self.player.y
+            ):
+                enemy.alive = False
+
+                blast_list.append(
+                    Blast(
+                        self.player.x + PLAYER_WIDTH / 2,
+                        self.player.y + PLAYER_HEIGHT / 2,
+                    )
+                )
+                pyxel.play(1, 1)
+                self.life -= 1
+                if self.life <= 0:
+                    self.scene = SCENE_GAMEOVER
+
+        self.player.update()
+        update_list(bullet_list)
+        update_list(enemy_list)
+        update_list(blast_list)
+
+        cleanup_list(enemy_list)
+        cleanup_list(bullet_list)
+        cleanup_list(blast_list)
+
+    def update_gameover_scene(self):
+        update_list(bullet_list)
+        update_list(enemy_list)
+        update_list(blast_list)
+
+        cleanup_list(enemy_list)
+        cleanup_list(bullet_list)
+        cleanup_list(blast_list)
+
+        # リトライ
+        if pyxel.btnp(pyxel.KEY_RETURN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_B):
+            self.scene = SCENE_PLAY
+            self.player.x = pyxel.width / 2
+            self.player.y = pyxel.height - 20
+            self.score = 0
+            self.life = 3
+
+            enemy_list.clear()
+            bullet_list.clear()
+            blast_list.clear()
+
+    def draw(self):
+        pyxel.cls(0)
+
+        self.background.draw()
+
+        if self.scene == SCENE_TITLE:
+            self.draw_title_scene()
+        elif self.scene == SCENE_PLAY:
+            self.draw_play_scene()
+        elif self.scene == SCENE_GAMEOVER:
+            self.draw_gameover_scene()
+
+        pyxel.text(4, 4, "SCORE {:5}".format(self.score), 7)
+        pyxel.text(80, 4, "LIFE {:3}".format(self.life), 7)
+
+    def draw_title_scene(self):
+        pyxel.text(45, 66, "Shooting", pyxel.frame_count % 16)
+        pyxel.text(31, 126, "PRESS ENTER or B", 14)
+
+    def draw_play_scene(self):
+        self.player.draw()
+        draw_list(bullet_list)
+        draw_list(enemy_list)
+        draw_list(blast_list)
+
+    def draw_gameover_scene(self):
+        draw_list(bullet_list)
+        draw_list(enemy_list)
+        draw_list(blast_list)
+
+        pyxel.text(43, 66, "GAME OVER", 8)
+        pyxel.text(20, 126, "RETRY PRESS ENTER or B", 12)
+
+
+App()
